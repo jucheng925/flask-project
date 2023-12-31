4436be449a7d9af14d6b09dc0649f037692b7782
Julie Cheng <jucheng925@gmail.com>
2023-12-31 18:25:01 -0500

    initial commit

---
+++ server/migrations/versions/90b7779b324d_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 90b7779b324d
+Revises: 
+Create Date: 2023-12-31 18:24:43.121648
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '90b7779b324d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
